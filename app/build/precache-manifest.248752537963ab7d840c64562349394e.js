2bc26ede89633cf04fee0f4b6b30cbe8578276f7
Max von Wolff <maxvwolff@googlemail.com>
2020-05-21 18:41:03 +0200

    fix

---
+++ app/build/precache-manifest.248752537963ab7d840c64562349394e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1422291fdbc619acd176380e17cccbed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b6bc3ce2176d5132b82",
+    "url": "/static/css/main.4d665fb3.chunk.css"
+  },
+  {
+    "revision": "57096451edbcc9a27da9",
+    "url": "/static/js/2.f382ecab.chunk.js"
+  },
+  {
+    "revision": "bba9dd905e309c17f1c9c9763497a1ec",
+    "url": "/static/js/2.f382ecab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b6bc3ce2176d5132b82",
+    "url": "/static/js/main.2275f699.chunk.js"
+  },
+  {
+    "revision": "a07415f3a612dc5038b4",
+    "url": "/static/js/runtime-main.2636bc25.js"
+  },
+  {
+    "revision": "2f42255ed88341aca3087d3ff17ad311",
+    "url": "/static/media/ding.2f42255e.mp3"
+  }
+]);
