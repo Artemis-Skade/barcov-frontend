1cf15854c1457c4006473cef81648df0f260eead
Max von Wolff <maxvwolff@googlemail.com>
2020-05-26 01:08:15 +0200

    added files

---
+++ app/build/precache-manifest.a78c489bd1e654e14569b0c1f4a72232.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b66836ce24d66f3ee62f97765ca68a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "173f471e945c91212fa3",
+    "url": "/static/css/main.ae3cf777.chunk.css"
+  },
+  {
+    "revision": "2c1e930e0ce4413107e2",
+    "url": "/static/js/2.123abd3b.chunk.js"
+  },
+  {
+    "revision": "bba9dd905e309c17f1c9c9763497a1ec",
+    "url": "/static/js/2.123abd3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "173f471e945c91212fa3",
+    "url": "/static/js/main.bc790729.chunk.js"
+  },
+  {
+    "revision": "a07415f3a612dc5038b4",
+    "url": "/static/js/runtime-main.2636bc25.js"
+  },
+  {
+    "revision": "2f42255ed88341aca3087d3ff17ad311",
+    "url": "/static/media/ding.2f42255e.mp3"
+  }
+]);
