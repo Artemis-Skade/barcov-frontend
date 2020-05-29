99abc33582cd2d8905cdbca952be47e1a544575c
Max von Wolff <maxvwolff@googlemail.com>
2020-05-29 21:27:57 +0200

    Changed login msg2

---
+++ app/build/precache-manifest.8286eec7f84162c5ca2248959bd2de7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9341ed0e0c13ef183c14baa344c5d6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e186e918e13ddf20bbc",
+    "url": "/static/css/main.ae3cf777.chunk.css"
+  },
+  {
+    "revision": "2c1e930e0ce4413107e2",
+    "url": "/static/js/2.123abd3b.chunk.js"
+  },
+  {
+    "revision": "bba9dd905e309c17f1c9c9763497a1ec",
+    "url": "/static/js/2.123abd3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e186e918e13ddf20bbc",
+    "url": "/static/js/main.56475446.chunk.js"
+  },
+  {
+    "revision": "a07415f3a612dc5038b4",
+    "url": "/static/js/runtime-main.2636bc25.js"
+  },
+  {
+    "revision": "2f42255ed88341aca3087d3ff17ad311",
+    "url": "/static/media/ding.2f42255e.mp3"
+  }
+]);
