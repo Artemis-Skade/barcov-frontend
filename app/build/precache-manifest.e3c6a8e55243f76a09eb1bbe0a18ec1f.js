5f7bf376d5b57e666fdc1d08bf59957324c4d53c
Max von Wolff <maxvwolff@googlemail.com>
2020-06-11 00:19:39 +0200

    Built

---
+++ app/build/precache-manifest.e3c6a8e55243f76a09eb1bbe0a18ec1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4b6d1f02a4ad4b599cbac0a6b305d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5650841d5dbfc711bbc2",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "42a977e091d1065b0b58",
+    "url": "/static/css/main.b553aa10.chunk.css"
+  },
+  {
+    "revision": "5650841d5dbfc711bbc2",
+    "url": "/static/js/2.b960ea99.chunk.js"
+  },
+  {
+    "revision": "9f5290274863d6e261cbad267bed5bfd",
+    "url": "/static/js/2.b960ea99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a977e091d1065b0b58",
+    "url": "/static/js/main.0986746d.chunk.js"
+  },
+  {
+    "revision": "a07415f3a612dc5038b4",
+    "url": "/static/js/runtime-main.2636bc25.js"
+  },
+  {
+    "revision": "2f42255ed88341aca3087d3ff17ad311",
+    "url": "/static/media/ding.2f42255e.mp3"
+  },
+  {
+    "revision": "857d625bd06a01bac723d7a7b7972b0c",
+    "url": "/static/media/flyertemplate.857d625b.png"
+  }
+]);
