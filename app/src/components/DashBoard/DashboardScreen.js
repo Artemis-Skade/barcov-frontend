f999be1b27f9700e42bb279d9cc7ff3ec6a3893a
Pascal Nießner <pniessner@orga-soft.de>
2020-08-31 19:36:59 +0200

    add EditCompanyModal.js and CompanyList.js

--- app/src/components/DashBoard/DashboardScreen.js
+++ app/src/components/DashBoard/DashboardScreen.js
@@ -1,33 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Cookies from 'universal-cookie';
-import settingsIcon from '../assets/img/settings.png';
-import settingsIconBlack from '../assets/img/settingsIconBlack.png';
-import Pencilimg from '../assets/img/pencil.png';
-import addIconBlack from '../assets/img/addIconBlack.png';
-import logobarcov from '../assets/img/logo.png';
-import listIcon from '../assets/img/listIcon.png';
-import closeIcon from '../assets/img/closeIcon.png';
-import torrilogo from '../assets/img/torrilogo.jpeg';
-import emilslogo from '../assets/img/emilslogo.png';
-import flyerpreview from "../assets/img/flyertemplate.png";
-import personIcon from "../assets/img/personIcon.png";
-import storeIcon from "../assets/img/storeIcon.png";
-
-import DatePicker from "react-datepicker";
+import closeIcon from '../../assets/img/closeIcon.png';
+import personIcon from "../../assets/img/personIcon.png";
 import sjcl from 'sjcl';
- 
+
 import "react-datepicker/dist/react-datepicker.css";
 
-import '../App.css';
-import '../Dashboard.css';
+import '../../App.css';
+import '../../Dashboard.css';
+import CompanyList from "./CompanyList";
+import EditCompanyModal from "./EditCompanyModal";
 
 let companies, setCompanies;
-let popUpOpen, setPopUpOpen;
 let pwPopUpOpen, setPwPopUpOpen;
 let eMailPopUpOpen, setEMailPopUpOpen;
 let image, setImage;
 let data, setData;
-let activeCompany, setActiveCompany;
 
 function logout() {
     const cookies = new Cookies();
@@ -93,7 +81,7 @@ function MyData() {
         }
 
         console.log(toSend);
-        
+
         fetch('https://' + window.Vars.domain + ':5000/change_owner_data', {
                 method: 'POST',
                 headers: {
@@ -103,7 +91,7 @@ function MyData() {
             }).then(res => res.json()).then(res => {
                 // Send other persons
                 console.log(res);
-                
+
                 let success = res["success"];
 
                 if (success) {
@@ -138,7 +126,7 @@ function MyData() {
             <p>Telefonnummer</p>
             <input type="text" name="mobile" value={data.data.mobile} onChange={(e) => changeVal(e)}/>
         </div>
-        
+
         <div className="links">
             <a onClick={() => setPwPopUpOpen(true)}>Passwort ändern</a> • <a onClick={() => setEMailPopUpOpen(true)}>E-Mail ändern</a>
         </div>
@@ -148,176 +136,14 @@ function MyData() {
         );
 }
 
-function MyCompanies(props) {
-    let companyRender = [];
-    let key = 0;
 
-    for (let company of data.data.companies) {
-        let logo = "";
-
-        if (key === 1) {
-            logo = emilslogo;
-        } else {
-            logo = torrilogo;
-        }
-
-        companyRender.push(<div className="company" key={key}>
-            <img src={"https://" + window.Vars.domain + ":5000/dashboard_icon?id=" + company.id} className="companyLogo"/>
-            <p>{company.name}</p>
-            <a href={"/data/" + company.id}>
-                <div className="dataBtn">
-                    <img src={listIcon} className="listIcon"/>
-                    <p>Gästeliste</p>
-                </div>
-            </a>
-            {false && <img src={settingsIcon} className="settingsIcon" onClick={() => {props.setPopUpOpen(true); setActiveCompany(key)}}/>}
-            <img src={settingsIcon} className="settingsIcon" onClick={() => alert("Diese Funktion kommt bald!")}/>
-        </div>);
-
-        key++;
-    }
-
-    return (<div className="Card CompanyCard">
-        <img src={storeIcon}/><h2>Meine Betriebe</h2>
-
-        {companyRender}
-
-        <div className="addbutton" onClick={() => alert("Diese Funktion kommt bald!")}><img src={addIconBlack}></img>Betrieb hinzufügen</div>
-    </div>
-        );
-}
 
 function changePassword() {
     console.log("Sending change password link...");
 }
 
-function handleChange(e) {
-    if (e.target.files.length) {
-        if(e.target.files[0].size > 5242880){
-            alert("Die Logo-Datei ist größer als 5 MB. Bitte lade eine kleinere Datei hoch.");
-            return;
-        }
-        console.log("Uploaded image");
-        console.log(e.target.files[0]);
-        setImage({
-          preview: URL.createObjectURL(e.target.files[0]),
-          raw: e.target.files[0]
-        });
-    }
-}
-
-function CompanyPopUp(props) {
-    let classNames = "CompanyPopUp";
-    console.log("Company PopUp Opened: " + props.opened);
-
-    if (!props.opened) {
-        classNames += " CompanyPopUpClosed";
-    }
-
-    let companyData = data.data.companies[activeCompany];
-
-    if (!popUpOpen) {
-        return (<></>);
-    }
-
-    return (<div className={classNames}>
-        <img src={closeIcon} className="closeIcon" onClick={() => setPopUpOpen(false)}/>
-        <h2>Betrieb bearbeiten</h2>
-
-        <div className="dataBlock">
-            <h3>Unternehmensadresse</h3>
-
-            <div className="inputField">
-                <p>Kurzname des Betriebs</p>
-                <input type="text" value={companyData.name}/>
-            </div>
-
-            <div className="inputField">
-                <p>Straße und Hausnnr.</p>
-                <input type="text" value={companyData.street}/>
-            </div>
-            <div className="inputField">
-                <p>PLZ</p>
-                <input type="text" value={companyData.zip}/>
-            </div>
-            <div className="inputField">
-                <p>Ort</p>
-                <input type="text" value={companyData.town}/>
-            </div>
-        </div>
-
-        <div className="dataBlock">
-            <h3>Rechnungsadresse</h3>
-
-            <div className="inputField">
-                <p>Rechtlicher Name des Betriebs</p>
-                <input type="text" value={companyData.rname}/>
-            </div>
-            <div className="inputField">
-                <p>Straße und Hausnnr.</p>
-                <input type="text" value={companyData.rstreet}/>
-            </div>
-            <div className="inputField">
-                <p>PLZ</p>
-                <input type="text" value={companyData.rzip}/>
-            </div>
-            <div className="inputField">
-                <p>Ort</p>
-                <input type="text" value={companyData.rtown}/>
-            </div>
-        </div>
-
-        <div className="dataBlock">
-            <h3>Zusätzliches</h3>
-
-            <div className="inputField">
-                <p>Link zu Speisekarte</p>
-                <input type="text" value={companyData.menu_link}/>
-            </div>
-            <div className="inputField">
-                <p>Link zu Instagram</p>
-                <input type="text" value={companyData.instagram}/>
-            </div>
-        </div>
-
-        <div className="dataBlock">
-            <h3>Firmenlogo</h3>
-
-            <div className="FileUpload FileUploadChange">
-                <div className="BigButton">
-                    <input
-                        type="file"
-                        className="UploadBtn"
-                        accept="image/x-png,image/png,image/jpeg,image/jpg"
-                        onChange={handleChange}
-                        for="files"
-                    />
-                    <p>Logo hochladen</p>
-                </div>
-
-                <div className="BigButtonDownload" onClick={() => alert("Kommt bald!")}>
-                    <p>Flyer herunterladen</p>
-                </div>
-
-                <div className="UploadPreview">
-                    <img src={flyerpreview}/>
-                    <img src={image.preview} className="Logoimgcenter"/>
-                    <img src={image.preview} className="Logoimgcorner"/>
-                </div>
-                <p className="previewText">Flyer-Vorschau</p>
-            </div>
-        </div>
 
 
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-
-        <div className="button">Speichern</div>
-    </div>);
-}
 
 function ChangePwPopUp(props) {
     let [passwords, setPasswords] = React.useState({old_pw: "", pw_new1: "", pw_new2: ""});
@@ -357,7 +183,7 @@ function ChangePwPopUp(props) {
                 // Send other persons
                 console.log(res);
                 console.log("Updated password.");
-                
+
                 let success = res["success"];
 
                 if (success) {
@@ -442,7 +268,7 @@ function ChangeEMailPopUp(props) {
                 // Send other persons
                 console.log(res);
                 console.log("Updated password.");
-                
+
                 let success = res["success"];
 
                 if (success) {
@@ -492,12 +318,21 @@ function ChangeEMailPopUp(props) {
 function DashboardScreen () {
     const cookies = new Cookies();
     [companies, setCompanies] = React.useState([]);
-    [popUpOpen, setPopUpOpen] = React.useState(false);
+    const [popUpOpen, setPopUpOpen] = React.useState(false);
     [pwPopUpOpen, setPwPopUpOpen] = React.useState(false);
     [eMailPopUpOpen, setEMailPopUpOpen] = React.useState(false);
     [image, setImage] = React.useState({ preview: "", raw: ""});
     [data, setData] = React.useState({data: {fname: "fname", lname: "lname", mobile: "123", mail: "123@123.de", companies: []}});
-    [activeCompany, setActiveCompany] = React.useState(0);
+    const [activeCompany, setActiveCompany] = React.useState(null);
+
+    useEffect(() => {
+        if (activeCompany !== null) {
+            setPopUpOpen(true)
+        } else {
+            setPopUpOpen(false)
+        }
+    }, [activeCompany])
+
 
     React.useEffect(() => {
         let testCompanies = [{name: "Torri Eiscafe"}, {name: "Emil's Cafe"}];
@@ -529,25 +364,30 @@ function DashboardScreen () {
 
     return (
         <>
-        <CompanyPopUp opened={popUpOpen}/>
-        <ChangePwPopUp opened={pwPopUpOpen}/>
-        <ChangeEMailPopUp opened={eMailPopUpOpen}/>
-        <div className={wrapperStyles}>
-            <h1>Guten Tag, <strong>{data.data.fname} {data.data.lname}</strong>!</h1>
+            {
+                popUpOpen &&
+                <EditCompanyModal company={activeCompany} opened={popUpOpen} onSuccess={() => {
+                    loadData(cookies.get("sessionKeyCompany"));
+                    setActiveCompany(null)
+                }} onClose={() => setActiveCompany(null)}/>
+            }
+            <ChangePwPopUp opened={pwPopUpOpen}/>
+            <ChangeEMailPopUp opened={eMailPopUpOpen}/>
+            <div className={wrapperStyles}>
+                <h1>Guten Tag, <strong>{data.data.fname} {data.data.lname}</strong>!</h1>
 
-            <MyCompanies setPopUpOpen={setPopUpOpen}/>
-            
-            <MyData />
+                <CompanyList companies={data.data.companies} onEditProperty={setActiveCompany}/>
+                <MyData/>
 
-            <br />
-            <br />
+                <br/>
+                <br/>
 
-            <p>Kontakt zum Kundensupport (24/7 erreichbar): <strong>06742 8289977</strong></p>
+                <p>Kontakt zum Kundensupport (24/7 erreichbar): <strong>06742 8289977</strong></p>
 
-            <br />
-            <br />
-            <br />
-        </div>
+                <br/>
+                <br/>
+                <br/>
+            </div>
         </>
     );
 }
