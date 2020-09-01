351e3334402cbc221983c0c1af8f7a546c4eb847
Pascal Nießner <pniessner@orga-soft.de>
2020-09-01 20:44:04 +0200

    rebuild

---
+++ app/build/precache-manifest.1525a8e78d056096816a8d1aa38f007a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c5cb22358ecf4f8879d294e00f2c48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a93f95e912032edbba7f",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "b6f02f33d32bc4ab1a16",
+    "url": "/static/css/main.baeb018c.chunk.css"
+  },
+  {
+    "revision": "a93f95e912032edbba7f",
+    "url": "/static/js/2.cf4bb723.chunk.js"
+  },
+  {
+    "revision": "9f5290274863d6e261cbad267bed5bfd",
+    "url": "/static/js/2.cf4bb723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f02f33d32bc4ab1a16",
+    "url": "/static/js/main.77f3a277.chunk.js"
+  },
+  {
+    "revision": "a07415f3a612dc5038b4",
+    "url": "/static/js/runtime-main.2636bc25.js"
+  },
+  {
+    "revision": "2f42255ed88341aca3087d3ff17ad311",
+    "url": "/static/media/ding.2f42255e.mp3"
+  },
+  {
+    "revision": "857d625bd06a01bac723d7a7b7972b0c",
+    "url": "/static/media/flyertemplate.857d625b.png"
+  },
+  {
+    "revision": "c43490114c6b05540a17e840abba0b01",
+    "url": "/static/media/navigo.c4349011.woff"
+  }
+]);
